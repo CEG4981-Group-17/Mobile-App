8a33236d7d5047333659fdc4acf0334edd01fb2b
Nick Hirzel <nhirzel1@gmail.com>
2021-04-06 18:24:24 -0400

    adds enabling bluetooth to lock button

--- app/src/main/java/com/example/icmapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/icmapp/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.icmapp.ui.home;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +19,10 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.icmapp.R;
 
 public class HomeFragment extends Fragment {
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_DISCOVERY_BT = 2;
 
+    private BluetoothAdapter BA;
     private HomeViewModel homeViewModel;
     private ToggleButton lockToggleButton;
 
@@ -27,16 +32,19 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         // gets refs to elements in home view
+        BA = BluetoothAdapter.getDefaultAdapter();
         lockToggleButton = (ToggleButton) root.findViewById(R.id.lockToggleButton);
 
         // set callbacks
         lockToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    homeViewModel.lock();
+                    bluetoothSendLock();
                 } else {
-                    homeViewModel.unlock();
+                    bluetoothSendUnlock();
                 }
+                // update button with actual locked status, in case it failed
+                buttonView.setChecked(homeViewModel.isLocked());
             }
         });
 
@@ -44,5 +52,39 @@ public class HomeFragment extends Fragment {
         lockToggleButton.setChecked(homeViewModel.isLocked());
 
         return root;
+    }
+
+    public void bluetoothSendLock() {
+        bluetoothSetup();
+        // TODO: send lock message to connected device
+        homeViewModel.lock();
+    }
+
+    public void bluetoothSendUnlock() {
+        bluetoothSetup();
+        // TODO: send unlock message to connected device
+        homeViewModel.unlock();
+    }
+
+    public void bluetoothSetup() {
+        bluetoothEnable();
+        // TODO: pair a device if no required device exists
+        // TODO: connect to the appropriate paired device
+    }
+
+    public void bluetoothEnable() {
+        if (!BA.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, REQUEST_ENABLE_BT);
+        }
+    }
+
+    public void bluetoothDisable() {
+        BA.disable();
+    }
+
+    public void bluetoothDiscoverable() {
+        Intent getDiscoverable = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getDiscoverable, REQUEST_DISCOVERY_BT);
     }
 }
