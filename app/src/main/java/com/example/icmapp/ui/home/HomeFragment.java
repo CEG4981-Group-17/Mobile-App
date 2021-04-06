2c70a1dadab60f30c637ad5a81b5c9e50a1e690e
Nick Hirzel <nhirzel1@gmail.com>
2021-04-06 17:09:52 -0400

    lock toggle button callbacks

--- app/src/main/java/com/example/icmapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/icmapp/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,12 +19,29 @@ import com.example.icmapp.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private ToggleButton lockToggleButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // gets refs to elements in home view
+        lockToggleButton = (ToggleButton) root.findViewById(R.id.lockToggleButton);
+
+        // set callbacks
+        lockToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    homeViewModel.lock();
+                } else {
+                    homeViewModel.unlock();
+                }
+            }
+        });
+
+        // set initial state
+        lockToggleButton.setChecked(homeViewModel.isLocked());
 
         return root;
     }
